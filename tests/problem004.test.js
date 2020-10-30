e29323278d8efa008662201570a61d7cf25d3b48
joao Felipe Santoro Pinto <joao.santoro.pinto@concrete.com.br>
2020-10-30 12:43:04 -0300

    Add problem 4 solution

---
+++ tests/problem004.test.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect
+
+const { problem004 } = require('../problems/problem004.js')
+
+describe('Problem 004', function () {
+  it('Should return 906609 as the highest highest palindrome product of 3-digits number.', function () {
+    expect(problem004(3)).to.equal(906609)
+  })
+  it('Should return 9 as the highest highest palindrome product of 1-digit number.', function () {
+    expect(problem004(1)).to.equal(9)
+  })
+  it('Should return 9009 as the highest highest palindrome product of 2-digits number.', function () {
+    expect(problem004(2)).to.equal(9009)
+  })
+})
