bb5c3c65661b6197a928e619f076b595774b4c56
joao Felipe Santoro Pinto <joao.santoro.pinto@concrete.com.br>
2020-10-30 22:49:51 -0300

    Add problem 005

---
+++ problems/problem005.js
@@ -0,0 +1,55 @@
+// https://projecteuler.net/problem=5
+/*
+  2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+  What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+
+const { isPrime } = require('../helper/math')
+
+const solution = (numbers) => {
+  let dividers = []
+  let primeNumbers = [2]
+
+  numbers.sort((a, b) => { return a - b })
+  // getting all primes until highest element of the array
+  for (let i = 3; i <= numbers[numbers.length - 1]; i = i + 2) {
+    if (isPrime(i)) {
+      primeNumbers.push(i)
+    }
+  }
+
+  let index = 0
+  while (notAllOnes(numbers)) {
+    let isDivisible = false
+    numbers = numbers.map((element) => {
+      if (element % primeNumbers[index] === 0) {
+        isDivisible = true
+        return element / primeNumbers[index]
+      }
+      return element
+    })
+    if (isDivisible) {
+      dividers.push(primeNumbers[index])
+    } else {
+      index++
+    }
+  }
+  console.log('dividers', dividers)
+  return dividers.reduce((product, element) => product * element, 1)
+}
+
+const notAllOnes = (numbers) => {
+  let divided = false
+  numbers.forEach((element) => {
+    if (element !== 1) {
+      divided = true
+    }
+  })
+  return divided
+}
+
+module.exports = {
+  problem005 (numbers) {
+    return solution(numbers)
+  }
+}
