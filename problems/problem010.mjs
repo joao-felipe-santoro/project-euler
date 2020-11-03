1389eb02fb0a1cdf6a399a98ba72c869fe597514
joao Felipe Santoro Pinto <joao.santoro.pinto@concrete.com.br>
2020-11-02 22:52:25 -0300

    Solve problem010

---
+++ problems/problem010.mjs
@@ -0,0 +1,21 @@
+// https://projecteuler.net/problem=10
+/*
+  The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+  Find the sum of all the primes below two million.
+*/
+
+import { isPrime } from '../helper/math.mjs'
+
+const solution = (number) => {
+  let sum = 2
+  for (let i = 3; i <= number; i += 2) {
+    if (isPrime(i)) {
+      sum = sum + i
+    }
+  }
+  return sum
+}
+
+export function problem010 (number) {
+  return solution(number)
+}
