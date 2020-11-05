27c76d01cae04f950ee0d2dc4b703c04bb5434ad
joao Felipe Santoro Pinto <joao.santoro.pinto@concrete.com.br>
2020-11-05 12:43:58 -0300

    Fix problem020 math file import

--- problems/problem020.mjs
+++ problems/problem020.mjs
@@ -4,10 +4,10 @@
   For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800, and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
   Find the sum of the digits in the number 100!
 */
-import { fatorial } from '../helper/math.mjs'
+import { factorial } from '../helper/math.mjs'
 
 const solution = (num) => {
-  const fact = fatorial(num)
+  const fact = factorial(num)
   return sumDigits(fact)
 }
 
